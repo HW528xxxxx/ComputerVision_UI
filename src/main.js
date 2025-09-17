08d8e036b59938092a7355199dde3a58285d0466
施涵文 <o975o4733o@gmail.com>
2025-09-17 14:49:39 +0800

    調整設定新增log

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+console.log('🚀 正在執行的環境:', import.meta.env.MODE)
+console.log('📝 當前載入的資源:', Array.from(document.scripts).map(s => s.src).filter(Boolean))
+
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { createHtmlPlugin } from 'vite-plugin-html'
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    vue(),
-    ...(mode === 'production'
-      ? [
-          createHtmlPlugin({
-            inject: {
-              data: {
-                mainJs: '/assets/index.[hash].js' // build 後才替換
-              }
-            }
-          })
-        ]
-      : []),
-  ],
-  base: './',
-}))
+export default defineConfig({
+  plugins: [vue()],
+  base: './'
+})
