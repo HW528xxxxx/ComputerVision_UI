dec520536f52b1a6fd78ab81cf32b64ba5f93f8f
施涵文 <o975o4733o@gmail.com>
2025-09-17 14:34:30 +0800

    mode === 'production' 正式和測試環境artifact位置不同用參數判斷資源位置

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    vue(),
+    ...(mode === 'production'
+      ? [
+          createHtmlPlugin({
+            inject: {
+              data: {
+                mainJs: '/assets/index.[hash].js' // build 後才替換
+              }
+            }
+          })
+        ]
+      : []),
+  ],
   base: './',
-})
+}))
