9a652741021b93653bd8f9afcd7aafd57010bd93
施涵文 <o975o4733o@gmail.com>
2025-10-17 13:57:56 +0800

    新增IP同意modal

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <ImageUploader />
+  <!-- IP 同意彈窗 -->
+  <IpConsentModal v-model="ipConsentGiven" />
+
+  <ImageUploader v-if="ipConsentGiven" />
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import IpConsentModal from './components/IpConsentModal.vue'
 import ImageUploader from './components/ImageUploader.vue'
+
+const ipConsentGiven = ref(false)
 </script>
